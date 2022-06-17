f0866ffb184c62cf8b669ae9df822507fc744619
Joshua <88754789+JuszJo@users.noreply.github.com>
2022-06-17 09:07:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 window.onload = async () => {
     var charts = document.querySelectorAll(".chart")
+
+    var html = document.querySelector("html")
     
     var amount = []
+
+    var font = window.getComputedStyle(html).fontSize
+
+    font = font.slice(0, -2)
     
+    var size = 0
+
     try {
         var res = await fetch('data.json')
         
@@ -10,33 +18,44 @@ window.onload = async () => {
         
         for(i of data) {
             amount.push(i.amount)
+            if(i.amount > size) {
+                size = i.amount
+            }
         }
         
         for(j in charts) {
             if(typeof(charts[j]) == "object") {
                 var heights = 0
-                if(parseInt(amount[j]) > 55) {
-                    heights = 150
+                if(parseInt(amount[j]) == size) {
+                    heights = 150 / parseInt(font)
                 }
-                heights = parseInt(amount[j]) * 3
+                heights = ((parseInt(amount[j]) * 150) / size) / parseInt(font)
 
-                charts[j].style.height = `${heights}px`
+                charts[j].style.height = `${heights}rem`
             }
         }
 
         charts.forEach((value, key) => {
-            if(amount[key] > 50) {
-                //value.style.backgroundColor = "hsl(186, 34%, 60%);"
+            if(amount[key] == size) {
                 value.classList.add('big')
             }
             value.addEventListener('mouseover', () => {
                 var att = document.createAttribute("data")
                 att.value = `$${amount[key]}`
                 value.setAttributeNode(att)
+                if(amount[key] == size) {
+                    value.classList.remove('chart')
+                    value.classList.add('chart2')
+                }
                 value.classList.add('addchart')
             })
             value.addEventListener('mouseleave', () => {
+                if(amount[key] == size) {
+                    value.classList.add('chart')
+                    value.classList.remove('chart2')
+                }
                 value.classList.remove('addchart')
+                value.classList.add('chart')
             })
         })
 
